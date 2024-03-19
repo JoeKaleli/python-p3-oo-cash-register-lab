db265b2128fbfbffa4c308772a3a7e38302ce95a
JoeKaleli <kalelimburu9@gmail.com>
2024-03-19 11:35:34 +0300

    Done with lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self):
+        self.items = []
+        self.total_price = 0
+
+    def add_item(self, name, quantity, price):
+        self.items.append({"name": name, "quantity": quantity, "price": price})
+        self.total_price += quantity * price
+
+    def calculate_discount(self, discount_percentage):
+        discount_amount = self.total_price * (discount_percentage / 100)
+        discounted_price = self.total_price - discount_amount
+        return discounted_price
+
+    def void_last_transaction(self):
+        if self.items:
+            last_item = self.items.pop()
+            self.total_price -= last_item["quantity"] * last_item["price"]
+
+    def get_items(self):
+        return self.items
+
+    def get_total_price(self):
+        return self.total_price
